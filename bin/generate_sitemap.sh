29914d4a656a70943f448e3650bae58899cced18
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2023-08-22 18:07:31 +0200

    ChatGPT: List all files, read the files in .github/workflows and the files in bin/ . As a professional software developer, create a script that creates a sitemap.xml that contains all .md files (with suffix .md replaced by .html) and uses the last commit date changing a file as date, as the bin/makeindex.sh does.

---
+++ bin/generate_sitemap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Start the sitemap.xml file
+echo '<?xml version="1.0" encoding="UTF-8"?>' > sitemap.xml
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> sitemap.xml
+
+# For each .md file in the repository
+for file in $(find . -type f -name "*.md"); do
+    # Get the last commit date for the file
+    last_commit_date=$(git log -1 --format=%cd --date=format:'%Y-%m-%d' -- "$file")
+    
+    # Convert the .md file path to .html
+    html_path="${file%.md}.html"
+    
+    # Append the entry to the sitemap.xml
+echo "  <url>" >> sitemap.xml
+echo "    <loc>https://stoerr.github.io/til/$html_path</loc>" >> sitemap.xml
+echo "    <lastmod>$last_commit_date</lastmod>" >> sitemap.xml
+echo "  </url>" >> sitemap.xml
+done
+
+# Close the sitemap.xml file
+echo '</urlset>' >> sitemap.xml
