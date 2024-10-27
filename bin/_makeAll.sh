8ea891ff0664b971b1adf98110f159068b54283a
Dr. Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2024-10-27 18:08:29 +0100

    sort embeddings.json to have less changes

--- bin/_makeAll.sh
+++ bin/_makeAll.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 bin/generate_sitemap.sh
 bin/llmupdatedb.sh
-sqlite-utils rows db/llmsearch.db -c id -c embedding -c content -o id --json-cols embeddings > db/embeddings.json
+sqlite-utils rows db/llmsearch.db -c id -c embedding -c content -o id --json-cols embeddings | jq 'sort_by(.id)' > db/embeddings.json
